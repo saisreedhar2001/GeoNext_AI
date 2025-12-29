547c809baff40a613234bd5aa003ab45eea273d5
chindukuru naga sai sreedhar <61618600+saisreedhar2001@users.noreply.github.com>
2025-12-29 18:50:44 +0530

    Backend_final

--- backend/main.py
+++ backend/main.py
@@ -38,6 +38,7 @@ app.add_middleware(
         
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://geonextai.vercel.app",
         "https://globalmobilityintelligence.vercel.app",
     ],
     allow_credentials=True,
